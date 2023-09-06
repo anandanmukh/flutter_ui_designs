3bdc828805523a564cedc4c7dd0fa8437757d8b4
Anandan Mukherjee <anandanmukh@gmail.com>
2023-09-06 09:27:57 +0530

    Create home_page.dart

---
+++ whatsapp_ui/lib/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(),
+      ),
+    );
+  }
+}
